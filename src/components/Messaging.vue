e6909e7a7d94bfdb01b16c703fd59585a24d54f3
Scott Semtner <sjsemtner@gmail.com>
2021-01-04 22:36:42 -0800

    Some other design changes

--- src/components/Messaging.vue
+++ src/components/Messaging.vue
@@ -3,8 +3,11 @@
     <h1>Messaging</h1>
     <br>
 
-    <Message from="Scott Semtner" text="Message Text" side="left"/>
-    <Message from="Other Person" text="This is another message" side="right"/>
+    <div v-for="message in this.messages" v-bind:key="message.id">
+      <Message v-if="message.from === 'Scott Semtner'" :from="message.from" :text="message.text" side="right"/>
+<!--      <Message v-if="message.from === this.current" :from="message.from" :text="message.text" side="right"/>-->
+      <Message v-else :from="message.from" :text="message.text" side="left"/>
+    </div>
   </b-container>
 </template>
 
@@ -14,6 +17,33 @@ import Message from "@/components/Messaging/Message";
 export default {
   name: "Messaging",
   components: {Message},
+  data () {
+    return {
+      current: "Scott Semtner",
+      messages: [
+        {
+          id: 0,
+          from: "Scott Semtner",
+          text: "I am saying hello"
+        },
+        {
+          id: 1,
+          from: "Mitchell Long",
+          text: "This is another person responding"
+        },
+        {
+          id: 2,
+          from: "Mitchell Long",
+          text: "Hello how are you doing?"
+        },
+        {
+          id: 3,
+          from: "Scott Semtner",
+          text: "I am doing great!"
+        },
+      ]
+    }
+  }
 }
 </script>
 
--- src/components/Messaging/Message.vue
+++ src/components/Messaging/Message.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <b-row align-h="center" class="m-4">
-      <b-col v-if="side === 'left'" cols="2" md="4"/>
+      <b-col v-if="side === 'right'" cols="2" md="4"/>
 
       <b-col cols="10" md="8">
         <b-card title="Message">
-          <UserIcon v-if="side === 'left'" class="message-icon left-message-icon" size="50" :display-name="this.from"/>
+          <UserIcon v-if="side === 'left'" class="message-icon left-message-icon bg-primary" size="50" :display-name="this.from"/>
           <UserIcon v-if="side === 'right'" class="message-icon right-message-icon" size="50"
                     :display-name="this.from"/>
           <b-card-body>{{ text }}</b-card-body>
         </b-card>
       </b-col>
 
-      <b-col v-if="side === 'right'" cols="2" md="4"/>
+      <b-col v-if="side === 'left'" cols="2" md="4"/>
     </b-row>
 
     <br>
@@ -35,11 +35,11 @@ export default {
   top: 0;
 }
 .right-message-icon {
-  right: 0;
-  transform: translate(50%, -50%);
-}
-.left-message-icon {
   left: 0;
   transform: translate(-50%, -50%);
+}
+.left-message-icon {
+  right: 0;
+  transform: translate(50%, -50%);
 }
 </style>
